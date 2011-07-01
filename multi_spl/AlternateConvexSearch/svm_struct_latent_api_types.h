6570318a91960734539ca717c65228445dc87ac5
Kevin Miller <kjmiller@torch.Stanford.EDU>
2011-07-01 12:01:47 -0700

    about to roll back

--- multi_spl/AlternateConvexSearch/svm_struct_latent_api_types.h
+++ multi_spl/AlternateConvexSearch/svm_struct_latent_api_types.h
@@ -36,8 +36,9 @@ typedef struct point_and_descriptor {
 typedef struct image_kernel_cache {
   int num_points;
   POINT_AND_DESCRIPTOR * points_and_descriptors;
-  int * begins;
-  int * ends;
+  int num_unique_x_vals;
+  POINT_AND_DESCRIPTOR * x_begin_pads;
+  int * x_begin_indices;
 } IMAGE_KERNEL_CACHE;
 
 typedef struct label {
